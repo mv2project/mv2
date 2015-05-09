5c24c4b8f9231c63561f2a9665a07647afeee725
Marcel Singer <marcel.singer@live.de>
2015-05-09 13:01:27 +0200

    Extended the logger

--- core/src/de/iss/mv2/logging/Logger.java
+++ core/src/de/iss/mv2/logging/Logger.java
@@ -31,10 +31,42 @@ public interface Logger {
 	/**
 	 * Returns all log entries with a timestamp later than the given one.
 	 * @param minimumDate The minimum timestamp of the entries to return.
-	 * @return All log entries with a timestamp laten than the given one.
+	 * @return All log entries with a timestamp later than the given one.
 	 */
 	public Iterable<LogEntry> filter(Date minimumDate);
 	
+	/**
+	 * Adds an entry to this log.
+	 * @param entry The entry to log.
+	 */
+	public void push(LogEntry entry);
 	
+	/**
+	 * Creates a new log entry and adds it to this log.
+	 * @param type The log level type of the entry to create.
+	 * @param component The component affected by the log entry to create.
+	 * @param message The message of the log entry to create.
+	 * @return The created log entry.
+	 */
+	public LogEntry push(LogEntry type, String component, String message);
+	
+	
+	/**
+	 * Adds all given entries to this log.
+	 * @param entries The entries to add.
+	 */
+	public void pushAll(Iterable<LogEntry> entries);
+	
+	/**
+	 * Removes all cached log entries from this log.
+	 */
+	public void clear();
+	
+	/**
+	 * Removes a log entry from this log.
+	 * @param entry The entry to remove.
+	 * @return {@code true}, if the given log entry was present.
+	 */
+	public boolean delete(LogEntry entry);
 	
 }
