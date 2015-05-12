ea71ccebb726b804f9187022736def34fedb7f69
Marcel Singer <marcel.singer@live.de>
2015-05-12 10:17:37 +0200

    Added the content type declaration enum for a message field.

---
+++ core/src/de/iss/mv2/messaging/CONTENT_TYPE.java
@@ -0,0 +1,19 @@
+package de.iss.mv2.messaging;
+
+/**
+ * Defines the content type of {@link MessageField}.
+ * @author Marcel Singer
+ *
+ */
+public enum CONTENT_TYPE {
+
+	/**
+	 * Defines that a message field contains a string literal.
+	 */
+	STRING_LITERAL,
+	/**
+	 * Defines that a message field contains binary content.
+	 */
+	BINARY
+	
+}
