6862992be06f6c1e74b1bc25c493a9a1b3be2ea8
Marcel Singer <marcel.singer@live.de>
2015-05-19 11:17:37 +0200

    Added MailListControl

---
+++ client/src/de/iss/mv2/client/gui/MailListControl.java
@@ -0,0 +1,73 @@
+package de.iss.mv2.client.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JComponent;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListCellRenderer;
+
+import de.iss.mv2.client.data.MailMessage;
+import de.iss.mv2.client.data.MailStorage;
+import de.iss.mv2.client.data.MailStorageListener;
+
+/**
+ * A list to display a list of mails.
+ * @author Marcel Singer
+ *
+ */
+public class MailListControl extends JComponent implements MailStorageListener {
+
+	/**
+	 * The serial.
+	 */
+	private static final long serialVersionUID = -1780306309494780559L;
+
+	/**
+	 * Holds the store with the mails to display.
+	 */
+	private final MailStorage mailStorage;
+	
+	/**
+	 * Holds the list.
+	 */
+	private final JList<MailMessage> list = new JList<MailMessage>();
+	
+	/**
+	 * Creates a new instance of {@link MailListControl}.
+	 * @param mailStorage The mail storage with the mails to display.
+	 */
+	public MailListControl(MailStorage mailStorage){
+		this.mailStorage = mailStorage;
+		mailStorage.addChangeListener(this);
+		setLayout(new BorderLayout());
+		list.setCellRenderer(new ListCellRenderer<MailMessage>() {
+
+			@Override
+			public Component getListCellRendererComponent(
+					JList<? extends MailMessage> list, MailMessage value,
+					int index, boolean isSelected, boolean cellHasFocus) {
+				return new MailPreviewControl(value);
+			}
+		});
+		add(new JScrollPane(list), BorderLayout.CENTER);
+	}
+	
+	/**
+	 * Updates the list.
+	 */
+	private void updateList(){
+		DefaultListModel<MailMessage> model = new DefaultListModel<MailMessage>();
+		for(MailMessage mm : mailStorage) model.addElement(mm);
+		list.setModel(model);
+	}
+
+	@Override
+	public void storeChanged(MailStorage storage) {
+		updateList();
+	}
+	
+	
+}
