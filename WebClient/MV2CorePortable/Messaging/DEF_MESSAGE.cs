81a1d8b0a39756d0212792872e5c00b3ccd4876f
marcel.singer@live.de <marcel.singer@live.de>
2015-05-22 19:22:33 +0200

    Added default message types

--- WebClient/MV2CorePortable/Messaging/DEF_MESSAGE.cs
+++ WebClient/MV2CorePortable/Messaging/DEF_MESSAGE.cs
@@ -9,8 +9,40 @@ namespace ISS.MV2.Core.Messaging {
         private int identifier = -1;
         public int Identifier { get { return identifier; } }
 
+        public static readonly DEF_MESSAGE UNKNOWN = new DEF_MESSAGE(-1);
+        public static readonly DEF_MESSAGE OK = new DEF_MESSAGE(1);
+        public static readonly DEF_MESSAGE ENCRYPTED_MESSAGE = new DEF_MESSAGE(2);
+        public static readonly DEF_MESSAGE UNABLE_TO_RPOCESS = new DEF_MESSAGE(3);
+        public static readonly DEF_MESSAGE CERT_REQUEST = new DEF_MESSAGE(4);
+        public static readonly DEF_MESSAGE CERT_RESPONSE = new DEF_MESSAGE(5);
+        public static readonly DEF_MESSAGE SPACE_CREATION_REQUEST = new DEF_MESSAGE(6);
+        public static readonly DEF_MESSAGE SPACE_CREATION_RESPONSE = new DEF_MESSAGE(7);
+        public static readonly DEF_MESSAGE DOMAIN_NAMES_REQUEST = new DEF_MESSAGE(8);
+        public static readonly DEF_MESSAGE DOMAIN_NAMES_RESPONSE = new DEF_MESSAGE(9);
+        public static readonly DEF_MESSAGE HELLO = new DEF_MESSAGE(10);
+        public static readonly DEF_MESSAGE CLIENT_LOGIN_REQUEST = new DEF_MESSAGE(11);
+        public static readonly DEF_MESSAGE SERVER_LOGIN_RESPONSE = new DEF_MESSAGE(12);
+        public static readonly DEF_MESSAGE CLIENT_LOGIN_DATA = new DEF_MESSAGE(13);
+        public static readonly DEF_MESSAGE SERVER_LOGIN_RESULT = new DEF_MESSAGE(14);
+        public static readonly DEF_MESSAGE CLIENT_CERTIFICATE_REQUEST = new DEF_MESSAGE(15);
+        public static readonly DEF_MESSAGE CLIENT_CERTIFICATE_RESPONSE = new DEF_MESSAGE(16);
+        public static readonly DEF_MESSAGE MESSAGE_DELIVERY_REQUEST = new DEF_MESSAGE(17);
+        public static readonly DEF_MESSAGE MESSAGE_DELIVERY_RESPONSE = new DEF_MESSAGE(18);
+        public static readonly DEF_MESSAGE CONTENT_MESSAGE = new DEF_MESSAGE(19);
+        public static readonly DEF_MESSAGE MESSAGE_QUERY_REQUEST = new DEF_MESSAGE(20);
+        public static readonly DEF_MESSAGE MESSAGE_QUERY_RESPONSE = new DEF_MESSAGE(21);
+        public static readonly DEF_MESSAGE MESSAGE_FETCH_REQUEST = new DEF_MESSAGE(22);
+        public static readonly DEF_MESSAGE MESSAGE_FETCH_RESPONSE = new DEF_MESSAGE(23);
+        public static readonly DEF_MESSAGE KEY_PUT_REQUEST = new DEF_MESSAGE(24);
+        public static readonly DEF_MESSAGE KEY_PUT_RESPONSE = new DEF_MESSAGE(25);
+        public static readonly DEF_MESSAGE KEY_REQUEST = new DEF_MESSAGE(26);
+        public static readonly DEF_MESSAGE KEY_RESPONSE = new DEF_MESSAGE(27);
 
-        public static readonly DEF_MESSAGE[] WELL_KNOWN_MESSAGES = new DEF_MESSAGE[] {  };
+
+
+        public static readonly DEF_MESSAGE[] WELL_KNOWN_MESSAGES = new DEF_MESSAGE[] {UNKNOWN, OK, ENCRYPTED_MESSAGE, UNABLE_TO_RPOCESS, CERT_REQUEST, CERT_RESPONSE, SPACE_CREATION_REQUEST, SPACE_CREATION_RESPONSE, DOMAIN_NAMES_REQUEST, DOMAIN_NAMES_RESPONSE,
+        HELLO, CLIENT_LOGIN_REQUEST, SERVER_LOGIN_RESPONSE, CLIENT_LOGIN_DATA, SERVER_LOGIN_RESULT, CLIENT_CERTIFICATE_REQUEST, CLIENT_CERTIFICATE_RESPONSE, MESSAGE_DELIVERY_REQUEST, MESSAGE_DELIVERY_RESPONSE, CONTENT_MESSAGE, MESSAGE_QUERY_REQUEST, MESSAGE_QUERY_RESPONSE, 
+        MESSAGE_FETCH_REQUEST, MESSAGE_FETCH_RESPONSE, KEY_PUT_REQUEST, KEY_PUT_RESPONSE, KEY_REQUEST, KEY_RESPONSE};
 
         private DEF_MESSAGE(int identifier) {
             this.identifier = identifier;
