4866df5df771a01994fe1a755bd197deb27d0e04
marcel.singer@live.de <marcel.singer@live.de>
2015-05-22 14:21:53 +0200

    Completed KeyPutRequest and added a corresponding test

--- core/src/de/iss/mv2/messaging/KeyPutRequest.java
+++ core/src/de/iss/mv2/messaging/KeyPutRequest.java
@@ -17,22 +17,37 @@ public class KeyPutRequest extends MV2Message {
 	/**
 	 * Sets the hashed passphrase. This passphrase will be needed to request the clients private key.
 	 * @param passphrase The passphrase to set.
+	 * @throws IllegalArgumentException If the given passphrase is {@code null} or empty.
 	 */
-	public void setPassphrase(byte[] passphrase){
+	public void setPassphrase(byte[] passphrase) throws IllegalArgumentException{
+		if(passphrase == null || passphrase.length == 0) throw new IllegalArgumentException("The passphrase must not be null or empty.");
 		setMessageField(new MessageField(DEF_MESSAGE_FIELD.HASH_BINARY, passphrase), true);
 	}
 	
 	/**
 	 * Returns the hashed passphrase.
-	 * @return The hashed passphrase that will be needed to request the clients private key.
+	 * @return The hashed passphrase that will be needed to request the clients private key. If there is none this method will return {@code null}.
 	 */
 	public byte[] getPassphrase(){
 		return getFieldDataArrayValue(DEF_MESSAGE_FIELD.HASH_BINARY, null);
 	}
 	
-	/*
-	public void setPrivateKey(byte[] key){
-		
-	}*/
+	/**
+	 * Sets the clients encrypted private key.
+	 * @param key The clients encrypted private key.
+	 * @throws IllegalArgumentException Is thrown if the given key is {@code null} or empty.
+	 */
+	public void setPrivateKey(byte[] key) throws IllegalArgumentException{
+		if(key == null || key.length == 0) throw new IllegalArgumentException("The key must not be null or empty.");
+		setMessageField(new MessageField(DEF_MESSAGE_FIELD.CONTENT_BINARY, key), true);
+	}
+	
+	/**
+	 * Returns the clients encrypted private key.
+	 * @return The clients encrypted private key or {@code null} if there is none.
+	 */
+	public byte[] getPrivateKey(){
+		return getFieldDataArrayValue(DEF_MESSAGE_FIELD.CONTENT_BINARY, null);
+	}
 	
 }
