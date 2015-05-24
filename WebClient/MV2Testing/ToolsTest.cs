67431efb680f3c66cd57f749ba7d91eb1d0c28bc
marcel.singer@live.de <marcel.singer@live.de>
2015-05-24 20:40:29 +0200

    Added DateTimeTools

---
+++ WebClient/MV2Testing/ToolsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ISS.MV2;
+
+
+namespace ISS.MV2.Tests {
+    [TestClass]
+    public class ToolsTest {
+
+        [TestMethod]
+        public void TestDateTimeConversion() {
+            long value = 1432492138036;
+            DateTime dt = DateTimeTools.FromJavaUnixTime(value);
+            Assert.AreEqual(635680961380360000, dt.Ticks);
+            DateTime dt2 = DateTimeTools.FromJavaUnixTime(dt.ToJavaUnixTime());
+            Assert.AreEqual(dt, dt2);
+        }
+    }
+}
