f43ae95d0a88499f9bb42387c248e9ebcf331e3f
marcel.singer@live.de <marcel.singer@live.de>
2015-05-24 19:30:32 +0200

    Added ISignatureProvider and SHA256WithRSASignatureProvider

---
+++ WebClient/MV2CorePortable/Security/ISignatureProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+
+namespace ISS.MV2.Security {
+    public interface ISignatureProvider {
+
+        public byte[] Sign(Stream data, AsymmetricKeyParameter privateKey);
+
+        public bool Validate(Stream data, byte[] signature, AsymmetricKeyParameter publicKey);
+
+    }
+}
---
+++ WebClient/MV2CorePortable/Security/SHA256WithRSASignatureProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ISS.MV2.IO;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Crypto.Digests;
+
+
+namespace ISS.MV2.Security {
+    public class SHA256WithRSASignatureProvider : ISignatureProvider {
+
+
+        public byte[] Sign(System.IO.Stream data, Org.BouncyCastle.Crypto.AsymmetricKeyParameter privateKey) {
+            Sha256Digest shaDigest = new Sha256Digest();
+            RsaDigestSigner signer = new RsaDigestSigner(shaDigest);
+            signer.Init(true, privateKey);
+            data.Iterate(new StreamExtension.BinIterationDelegate((b) => {
+                signer.Update(b);
+            }));
+            return signer.GenerateSignature();
+        }
+
+        public bool Validate(Stream data, byte[] signature, Org.BouncyCastle.Crypto.AsymmetricKeyParameter publicKey) {
+            Sha256Digest shaDigest = new Sha256Digest();
+            RsaDigestSigner signer = new RsaDigestSigner(shaDigest);
+            signer.Init(false, publicKey);
+            data.Iterate(new StreamExtension.BinIterationDelegate((b)=>{
+                signer.Update(b);
+            }));
+            return signer.VerifySignature(signature);
+        }
+    }
+}
