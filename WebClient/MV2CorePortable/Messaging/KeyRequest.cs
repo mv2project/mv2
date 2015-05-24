9bbb325bc74a9a5ada09cf4883bcf0961e73f120
marcel.singer@live.de <marcel.singer@live.de>
2015-05-24 21:28:43 +0200

    Added KeyRequest and -Response

---
+++ WebClient/MV2CorePortable/Messaging/KeyRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISS.MV2.Messaging {
+    public class KeyRequest : MV2Message {
+
+        public byte[] Passphrase {
+            get {
+                return GetFieldDataValue(DEF_MESSAGE_FIELD.HASH_BINARY, null);
+            }
+            set {
+                SetMessageField(DEF_MESSAGE_FIELD.HASH_BINARY, value);
+            }
+        }
+
+        public KeyRequest()
+            : base(DEF_MESSAGE.KEY_REQUEST) {
+        }
+
+    }
+}
---
+++ WebClient/MV2CorePortable/Messaging/KeyResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISS.MV2.Messaging {
+    public class KeyResponse : MV2Message{
+
+        public byte[] Key {
+            get {
+                return GetFieldDataValue(DEF_MESSAGE_FIELD.CONTENT_BINARY, null);
+            }
+            set {
+                SetMessageField(DEF_MESSAGE_FIELD.CONTENT_BINARY, value);
+            }
+        }
+
+        public KeyResponse()
+            : base(DEF_MESSAGE.KEY_RESPONSE) {
+        }
+
+    }
+}
