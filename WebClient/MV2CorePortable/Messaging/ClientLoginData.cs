e4df381288d6ec45954beebddcb86daf92aca208
marcel.singer@live.de <marcel.singer@live.de>
2015-05-24 12:34:49 +0200

    Added ClientLoginData

---
+++ WebClient/MV2CorePortable/Messaging/ClientLoginData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISS.MV2.Messaging {
+    public class ClientLoginData : MV2Message {
+
+
+        public byte[] DecryptedTestPhrase {
+            get {
+                return GetFieldDataValue(DEF_MESSAGE_FIELD.CONTENT_BINARY, null);
+            }
+            set {
+                SetMessageField(DEF_MESSAGE_FIELD.CONTENT_BINARY, value);
+            }
+        }
+
+
+
+        public ClientLoginData()
+            : base(DEF_MESSAGE.CLIENT_LOGIN_DATA) {
+        
+        }
+
+    }
+}
